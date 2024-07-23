17d4cddc54151adae03a48a53bfc7d39fc32dc73
jidnyasa24 <jidnyasazope@gmail.com>
2024-07-23 12:50:57 +0530

    Initial commit

---
+++ Src_Code/Tabs/home.py
@@ -0,0 +1,13 @@
+# Import necessary modules
+import streamlit as st
+
+
+def app():
+    # This function create the home page
+
+    # Add title to the home page
+    st.title("Human depression Detection")
+
+    # Add image to the home page
+    st.image("home.jpg")
+
